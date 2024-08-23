4976c1502f51b8894fff64f418bfb7504941f6f2
whybe-choi <choi980912@khu.ac.kr>
2024-08-23 16:16:15 +0900

    feat: Rename source files

--- src/vectordb.py
+++ src/vectordb.py
@@ -30,10 +30,11 @@ def load_embed_model_from_local(model_path):
 def load_and_split_docs(file_path, tokenizer):
     """Load PDF file and split"""
     markdown_text = pymupdf4llm.to_markdown(file_path)
-    page_contents = markdown_text.split("-----")[:-1]
+    page_contents = markdown_text.split("\n-----\n")[:-1]
     documents = [Document(page_content=page_content) for page_content in page_contents]
 
-    print(f"문서의 총 토큰 수 : {len(tokenizer.encode(markdown_text))}")
+    print(f"문서의 총 페이지 수: {len(page_contents)}")
+    print(f"문서의 총 토큰 수: {len(tokenizer.encode(markdown_text))}")
 
     return documents
 
@@ -53,15 +54,23 @@ def create_vectorstore(chunks, model_path, title):
 
 def process_pdfs_from_dataframe(df, model_path, tokenizer):
     "Store DB using PDF names"
-    sources = df["Source"].unique()
+    sources = [unicodedata.normalize("NFC", source) for source in df["Source"].unique()]
 
     source_mapping = {}
-    for idx, source in enumerate(df["Source"].unique()):
-        source = unicodedata.normalize("NFC", source)
+    for idx, source in enumerate(sources):
         source_mapping[source] = f"test{idx+1}"
 
+    # Rename PDF files
+    test_file_paths = os.listdir("./data/test_source")
+
+    for path in test_file_paths:
+        old_path = os.path.join("./data/test_source", path)
+        source = unicodedata.normalize("NFC", path.rstrip(".pdf"))
+        new_path = os.path.join("./data/test_source", f"{source_mapping[source]}.pdf")
+        if os.path.exists(old_path):
+            os.rename(old_path, new_path)
+
     for source in tqdm(sources, desc="Processing PDFs"):
-        source = unicodedata.normalize("NFC", source)
         file_path = f"./data/test_source/{source_mapping[source]}.pdf"
 
         print(f"Processing {source}...")
